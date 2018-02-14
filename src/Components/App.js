dc9ef3a93dbd6d6466c2777918fcacade265b665
Marco Castillo <marcocastillo2506@gmail.com>
2018-02-14 00:56:19 -0600

    Added Component App and Zip Code tab

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+import ZipForm from './ZipForm';
+
+class App extends React.Component{
+  render(){
+    return( <div className="app">
+      <ZipForm />
+    </div>
+  );
+  }
+}
+
+export default App;
